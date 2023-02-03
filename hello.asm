146db47c6c075fa32d12f8855f0e17da2dbdb4f7
probitie <viacheslav.mashkov@gmail.com>
2023-02-03 05:13:34 +0200

    ))

--- hello.asm
+++ hello.asm
@@ -1,15 +1,11 @@
 section .data
-    text db "Hello, World!",10
+    text db "Hello, Wuuld!",10
 
 section .text
     global _start
 
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 14
-    syscall
+    jmp _start
 
     mov rax, 60
     mov rdi, 0
